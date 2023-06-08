6d3add6fbae984c2e58b3291c861a5dc72cb645e
MemoonaTareen <memoona.tareen1@gmail.com>
2023-06-09 00:17:04 +0500

    main.js file

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 
 let list = document.querySelectorAll(".navigation li");
-
 function activeLink() {
   list.forEach((item) => {
     item.classList.remove("hovered");
   });
   this.classList.add("hovered");
 }
-
 list.forEach((item) => item.addEventListener("mouseover", activeLink));
-
-
 let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
 let main = document.querySelector(".main");
